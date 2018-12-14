dbf152b2d0f3fbeaaa6fc6b172b349ebfa265f01
commanderking <jek582@mail.harvard.edu>
2018-12-13 20:24:39 -0500

    first commit

---
+++ Assets/CreateFireball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFireball : MonoBehaviour {
+    public GameObject bowserTarget;
+    public GameObject fireball; 
+    private readonly int countOfFireballs;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Create() {
+        if(bowserTarget != null)
+        {
+            fireball.transform.parent = bowserTarget.transform;
+        }
+    }
+}
---
+++ Assets/PlayFireBallSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayFireBallSound : MonoBehaviour {
+    private AudioSource audio;
+	// Use this for initialization
+	void Start () {
+        audio = this.GetComponent<AudioSource>();
+        audio.Play();
+           // Debug.Log("Hey");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
